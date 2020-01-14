a5fafee0c62060561dfcd7057c59512a16daf5f4
daaaanil81 <daaaanil81@gmail.com>
2020-01-14 17:30:18 +0200

    Add Icons in tool-bar

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,3 +1,6 @@
+import React, {Component} from 'react';
+import { Icon } from 'react-native-elements'
+import { Image } from 'react-native-elements'
 import { createAppContainer } from 'react-navigation' //Подключаем некий контейнер в который мы позже засунем наш тулбар
 import { createBottomTabNavigator } from 'react-navigation-tabs' //Функция которая создает Тулбар
 
@@ -10,12 +13,41 @@ const AppNavigator = createBottomTabNavigator({ //Создаем тулбар
     Fav: {
         //screen отвечает за действие после нажатия, в данном примере он открывает страницу Fav
         screen: Fav,
+        navigationOptions: () => ({
+            tabBarIcon: ({tintColor}) => (
+                <Icon
+                    name="stars"
+                    color={'grey'}
+                    size={35}
+                />
+            )
+        })
     },
     Feed: {
         screen: Feed,
+        navigationOptions: () => ({
+            tabBarIcon: ({ focused }) => {
+                return (
+                    <Image 
+                        source={require('../pictures/events.png')}
+                        style={{width: 35, height: 35}}
+                    />
+                )
+            }
+        })
     },
     Profile: {
         screen: Profile,
+        navigationOptions: () => ({
+            tabBarIcon: ({ focused }) => {
+                return (
+                    <Image 
+                        source={require('../pictures/profile.png')}
+                        style={{width: 35, height: 35}}
+                    />
+                )
+            }
+        })
     }
 })
 
