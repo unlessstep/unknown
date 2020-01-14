f6cead6bf617f317143848cd767da0e00188d74c
Alex <alex@MacBook-Air-Alex.local>
2020-01-14 04:47:10 +0200

    ToolBar + Menu

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from "./navigation/AppNavigator";
+
+const App = () => {
+  return (
+      <AppNavigator/>
+  )
+}
+
+export default App
